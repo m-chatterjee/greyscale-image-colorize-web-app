af1b495721aa9e1ac9543981eb5bfad9b45dd45d
Mainak <mainakc652@gmail.com>
2021-06-25 06:14:20 +0530

    nothing changed -front end work left & not all models done

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def predict(category):
     imsave('static/results/result.jpg', lab2rgb(result))
 
 if __name__=='__main__':
-    app.run(debug=True,host="192.168.0.102",port=5000)
+    app.run(debug=True,host="0.0.0.0",port=80)
